15fcfc575b2b28aa0d35ca34d48e3aaeb5f2aaf2
Alex Cocan <al_razvan@yahoo.com>
2024-02-14 13:51:06 +0100

    adds minor changes to cli

--- cli.js
+++ cli.js
@@ -2,31 +2,24 @@
 
 const { execSync } = require('child_process');
 const { join } = require('path');
-const fs = require('fs');
 const { promisify } = require('util');
+const ncp = require('ncp').ncp;
+const copy = promisify(ncp);
 const rimraf = promisify(require('rimraf'));
 
 (async () => {
-  try {
-    const targetDirectory = process.argv[2] || process.cwd();
-    const projectName = 'react-register-login-fullstack'; 
-    const finalPath = join(targetDirectory, projectName);
+    const targetDirectory = process.argv[2] || process.cwd(); 
+    console.log(`Creating a new React register/login project in ${targetDirectory}...`);
 
-    if (fs.existsSync(finalPath)) {
-      console.error(`A directory named ${projectName} already exists in ${targetDirectory}. Please choose a different name or location.`);
-      process.exit(1);
+    const projectPath = join(targetDirectory, 'react-register-login-fullstack');
+    
+    try {
+        execSync('git clone https://github.com/furbo1/react-register-login-fullstack.git', { stdio: 'inherit', cwd: targetDirectory });
+        
+        await rimraf(join(projectPath, '.git'));
+
+        console.log('Project created successfully.');
+    } catch (error) {
+        console.error('Failed to create the project:', error);
     }
-
-    console.log(`Creating a new React register/login project in ${finalPath}...`);
-
-    execSync(`git clone https://github.com/furbo1/react-register-login-fullstack.git "${projectName}"`, { stdio: 'inherit', cwd: targetDirectory });
-
-    await rimraf(join(finalPath, '.git'));
-
-    console.log('Project created successfully.');
-  } catch (error) {
-    console.error('Failed to create the project:', error.message);
-    process.exit(1);
-  }
 })();
-
